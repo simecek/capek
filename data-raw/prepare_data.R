88be0326f159d0f668e1f45c9ab2f1dece84de0f
simecek <lamparna@gmail.com>
2017-05-08 14:43:34 -0400

    encoding is now UTF-8, only actual text used, the rest cutted out

--- data-raw/prepare_data.R
+++ data-raw/prepare_data.R
@@ -6,12 +6,20 @@ library(devtools)
 
 ## Download the books
 
-tovarna_na_absolutno <- read_lines("https://web2.mlp.cz/koweb/00/03/34/75/61/tovarna_na_absolutno.txt")
-krakatit <- read_lines("https://web2.mlp.cz/koweb/00/03/34/75/44/krakatit.txt")
-hordubal <- read_lines("https://web2.mlp.cz/koweb/00/03/34/75/34/hordubal.txt")
-povetron <- read_lines("https://web2.mlp.cz/koweb/00/03/34/75/59/povetron.txt")
-obycejny_zivot <- read_lines("https://web2.mlp.cz/koweb/00/03/34/75/54/obycejny_zivot.txt")
-valka_s_mloky <- read_lines("https://web2.mlp.cz/koweb/00/03/34/75/66/valka_s_mloky.txt")
+tovarna_na_absolutno0 <- read_lines("https://web2.mlp.cz/koweb/00/03/34/75/61/tovarna_na_absolutno.txt")
+krakatit0 <- read_lines("https://web2.mlp.cz/koweb/00/03/34/75/44/krakatit.txt")
+hordubal0 <- read_lines("https://web2.mlp.cz/koweb/00/03/34/75/34/hordubal.txt")
+povetron0 <- read_lines("https://web2.mlp.cz/koweb/00/03/34/75/59/povetron.txt")
+obycejny_zivot0 <- read_lines("https://web2.mlp.cz/koweb/00/03/34/75/54/obycejny_zivot.txt")
+valka_s_mloky0 <- read_lines("https://web2.mlp.cz/koweb/00/03/34/75/66/valka_s_mloky.txt")
+
+## Convert encoding from CP1250 to UTF-8, cutting out editor notes
+tovarna_na_absolutno <- iconv(tovarna_na_absolutno0, from = "CP1250", to = "UTF-8")[25:1149]
+krakatit <- iconv(krakatit0, from = "CP1250", to = "UTF-8")[10:2102]
+hordubal <- iconv(hordubal0, from = "CP1250", to = "UTF-8")[12:1195]
+povetron <- iconv(povetron0, from = "CP1250", to = "UTF-8")[10:816]
+obycejny_zivot <- iconv(obycejny_zivot0, from = "CP1250", to = "UTF-8")[11:674]
+valka_s_mloky <- iconv(valka_s_mloky0, from = "CP1250", to = "UTF-8")[11:1636]
 
 ## Now, add the data files to the package
 
